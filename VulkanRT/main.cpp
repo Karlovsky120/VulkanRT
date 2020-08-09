14d0db9e946a9d53b115f23b79f803fada6f6e82
Karlovsky120 <karlovsky120@gmail.com>
2020-08-09 20:58:12 +0200

    Add GLFW 3.3.2 to project. Create surface.

--- VulkanRT/main.cpp
+++ VulkanRT/main.cpp
@@ -1,15 +1,35 @@
 #define VOLK_IMPLEMENTATION
+#define GLFW_INCLUDE_VULKAN
 
 #include "volk.h"
+#include "glfw3.h"
 
 #include <assert.h>
+#include <cstdio>
 
 #define ARRAYSIZE(object) sizeof(object)/sizeof(object[0])
-
 #define VK_CHECK(call) assert(call == VK_SUCCESS)
 
+#define WIDTH 1280
+#define HEIGHT 720
+
 int main(int argc, char* argv[]) {
+
+	if (!glfwInit()) {
+		printf("Failed to initialize GLFW!");
+		return -1;
+	}
+
+	GLFWwindow* window;
+	window = glfwCreateWindow(WIDTH, HEIGHT, "VulkanRT", NULL, NULL);
+	if (!window) {
+		printf("Failed to create GLFW window!");
+		glfwTerminate();
+		return -1;
+	}
+
 	if (volkInitialize() != VK_SUCCESS) {
+		printf("Failed to initialize Volk!");
 		return -1;
 	}
 
@@ -46,6 +66,19 @@ int main(int argc, char* argv[]) {
 	VK_CHECK(vkCreateInstance(&instanceCreateInfo, 0, &instance));
 
 	volkLoadInstance(instance);
+
+	VkSurfaceKHR surface = 0;
+
+	if (!glfwCreateWindowSurface(instance, window, 0, &surface)) {
+		printf("Failed to create window surface!");
+		return -1;
+	}
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 
 	return 0;
 }
