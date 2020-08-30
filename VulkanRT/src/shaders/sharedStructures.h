62a42bf1fd79a5be507df4f6603fdb9a3ca6e522
Karlovsky120 <karlovsky120@gmail.com>
2020-08-30 12:20:13 +0200

    Disable warning for glm library in sharedStructures.h

--- VulkanRT/src/shaders/sharedStructures.h
+++ VulkanRT/src/shaders/sharedStructures.h
@@ -1,6 +1,9 @@
 #ifdef CPP_SHADER_STRUCTURE
+#pragma warning(push, 0)
+#include "glm/fwd.hpp"
 #include "glm/mat4x4.hpp"
 #include "glm/vec3.hpp"
+#pragma warning(pop)
 
 #define mat4 glm::mat4
 #define vec3 glm::vec3
